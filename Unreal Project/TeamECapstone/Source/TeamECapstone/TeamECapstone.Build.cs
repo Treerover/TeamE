71b4908ff5cc636adde42d6e19c32f2aa2c93a4d
Treerover <austin200111@hotmail.ca>
2023-09-18 22:01:18 -0400

    New Abyss game demo project.

---
+++ Unreal Project/TeamECapstone/Source/TeamECapstone/TeamECapstone.Build.cs
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TeamECapstone : ModuleRules
+{
+	public TeamECapstone(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateIncludePaths.AddRange(new string[] {
+            "TeamECapstone/Actors",
+            "TeamECapstone/Components",
+            "TeamECapstone/Enums",
+            "TeamECapstone/Gamemodes",
+            "TeamECapstone/Managers",
+            "TeamECapstone/Subsystems",
+            "TeamECapstone/Controllers",
+            "TeamECapstone/Actors/Abilities",
+            "TeamECapstone/Actors/Pawns",
+            "TeamECapstone/Actors/Weapons",
+            "TeamECapstone",
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
